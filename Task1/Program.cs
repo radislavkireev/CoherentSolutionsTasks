83359a8c01cb2c6b1fe66a8a14fdf49f71e3cccc
zylvis <71163258+zylvis@users.noreply.github.com>
2021-11-26 01:09:13 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RangeTernary2s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter int a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("enter int b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Converter converter = new Converter();
+            List<string> rangeTernaryItems = new List<string>();
+
+            if (a > b)
+            {
+                int c = a + b;
+                a = c - a;
+                b = c - b;
+            }
+
+            for (; a <= b; a++)
+            {
+                char[] ternaryItem = converter.DecimalToTernary(a).ToCharArray();
+                int count = 0;
+                for (int j = 0; j < ternaryItem.Length; j++)
+                {
+                    if (ternaryItem[j] == '2')
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 2)
+                {
+                    rangeTernaryItems.Add(converter.DecimalToTernary(a));
+                }
+
+            }
+
+            for (int i = 0; i < rangeTernaryItems.Count; i++)
+            {
+                Console.WriteLine(rangeTernaryItems[i]);
+            }
+        }
+    }
+}
