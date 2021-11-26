c2f88916c25c6caf73b7b349230a33c9b0bbd29a
zylvis <71163258+zylvis@users.noreply.github.com>
2021-11-26 04:38:52 +0200

    Add project files.

---
+++ ISBNTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ISBNTask
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input 9 digit number: ");
+            
+            string number = Console.ReadLine();
+            StringBuilder strNumber = new StringBuilder();
+            int sum = 0;
+            int count = 10;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                strNumber.Append(number[i]);
+            }
+
+            int[] numberElements = new int[number.Length];
+
+            for (int i = 0; i < numberElements.Length; i++)
+            {
+                numberElements[i] = int.Parse(number[i].ToString());
+            }
+
+            foreach (var item in numberElements)
+            {
+                sum += item * count;
+                count--;
+            }
+
+            int remainder = sum % 11;
+            int checkDigit = 11 - remainder;
+
+            if (checkDigit != 10)
+            {
+                strNumber.Append(checkDigit.ToString());
+                Console.Write("IBSN: ");
+                foreach (var item in strNumber.ToString())
+                {
+                    Console.Write(item);
+                }
+            }
+            else
+            {
+                strNumber.Append("X");
+                Console.Write("IBSN: ");
+                foreach (var item in strNumber.ToString())
+                {
+                    Console.Write(item);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
