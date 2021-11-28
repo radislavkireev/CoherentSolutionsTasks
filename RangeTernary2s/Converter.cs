0140d3ae0daba8424a5a987e48e2139e4c655b38
Radislav Kireev <rkireev@lt.life>
2021-11-28 13:07:43 +0200

    Review

--- RangeTernary2s/Converter.cs
+++ RangeTernary2s/Converter.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+// RK: drop unused usings
 using System.Collections.Generic;
+// RK: Added this for "Reverse" method.
+using System.Linq;
 using System.Text;
 
 namespace RangeTernary2s
 {
+    //RK: add "internal" before "class" method, it is a good practice
+    // to explicitly set protection modifiers.
     class Converter
     {
         public string DecimalToTernary(int n)
@@ -16,6 +21,13 @@ namespace RangeTernary2s
                 str.Append(n % 3);
                 n /= 3;
             }
+
+            // RK: The transforms below could be replaced with that call
+            // return str.ToString().Reverse().ToString();
+            //
+            // Yeah, this is not "fair" as we have not discussed Linq
+            // yet, but anyway it is simpler. 
+
 
             char[] charArray = str.ToString().ToCharArray();
             Array.Reverse(charArray);
--- RangeTernary2s/Program.cs
+++ RangeTernary2s/Program.cs
@@ -5,6 +5,7 @@ namespace RangeTernary2s
 {
     class Program
     {
+        //RK: args is not used and could be dropped. 
         static void Main(string[] args)
         {
             Console.Write("enter int a: ");
@@ -15,6 +16,10 @@ namespace RangeTernary2s
             Converter converter = new Converter();
             List<string> rangeTernaryItems = new List<string>();
 
+
+            // RK: This is a smart way for swap, but prefferably
+            // would be to use standard code:
+            // int temp = a; a = b; b = temp;
             if (a > b)
             {
                 int c = a + b;
@@ -24,9 +29,15 @@ namespace RangeTernary2s
 
             for (; a <= b; a++)
             {
+                //RK: Search of "2"s could be simplified with
+                // string ternar = converter.DecimalToTernary(a);
+                // foreach(char c in ternar) ...
+                // 
+                //So, we do not need in ToCharArray call and j variable
                 char[] ternaryItem = converter.DecimalToTernary(a).ToCharArray();
+
                 int count = 0;
-                for (int j = 0; j < ternaryItem.Length; j++)
+                    for (int j = 0; j < ternaryItem.Length; j++)
                 {
                     if (ternaryItem[j] == '2')
                     {
@@ -34,6 +45,10 @@ namespace RangeTernary2s
                     }
                 }
 
+                //RK: There is no need to store result in rangeTernaryItems
+                // result could be printed immedeately as Console.WriteLine(ternar);
+                // Also, converter.DecimalToTernary is called twice,
+                // keep result in buffer variable to avoid extra computations.
                 if (count == 2)
                 {
                     rangeTernaryItems.Add(converter.DecimalToTernary(a));
@@ -41,6 +56,7 @@ namespace RangeTernary2s
 
             }
 
+            // RK: Just print result immedeately
             for (int i = 0; i < rangeTernaryItems.Count; i++)
             {
                 Console.WriteLine(rangeTernaryItems[i]);
